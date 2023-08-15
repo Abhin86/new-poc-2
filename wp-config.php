455b72043422c0a62c9da9f60cda235820bf1e09
Abhin86 <124777743+Abhin86@users.noreply.github.com>
2023-08-15 22:07:43 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'wordpress' );
 
 /** Database hostname */
-define( 'DB_HOST', '35.228.151.183' );
+define( 'DB_HOST', '35.228.38.170' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
